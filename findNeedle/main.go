26eb0b1c695ce51e7f18ce5fd327fb45ed614726
yogawahyudi7 <yogawahyudi7@gmail.com>
2023-05-16 21:22:57 +0700

    Fix: findNeedle - Change default result value & Add more example

--- findNeedle/main.go
+++ findNeedle/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func FindNeedle(haystack []string, needle string) int {
-	result := 0
+	result := -1
 
 	for i, v := range haystack {
 		if v == needle {
@@ -18,4 +18,6 @@ func FindNeedle(haystack []string, needle string) int {
 
 func main() {
 	fmt.Println(FindNeedle([]string{"red", "blue", "yellow", "black", "grey"}, "blue"))
+	fmt.Println(FindNeedle([]string{"red", "blue", "yellow", "black", "grey"}, "grey"))
+	fmt.Println(FindNeedle([]string{"red", "blue", "yellow", "black", "grey"}, "nol"))
 }
